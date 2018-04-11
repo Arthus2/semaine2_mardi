0245997a6f9ce71241c26cbd6559a3f8e43d8e57
Arthus Richard <arthusr@outlook.com>
2018-04-11 02:24:06 +0200

    mouai

--- 2.rb
+++ 2.rb
@@ -2,6 +2,7 @@ def cesar(string, number)
     string.each_byte do |byte|
     byte += number
     alphabet = ["a".."z"]
+    transfo = alphabet.rotate(number)
   end
   puts byte.chr.to_s
 end
---
+++ 3.rb
@@ -0,0 +1,9 @@
+def test
+  array = [17,3,6,9,15,8,6,1,10]
+  array.map.with_index do |n, i|
+    if n < array[i+1]
+      puts n
+    else 
+end
+
+puts test
